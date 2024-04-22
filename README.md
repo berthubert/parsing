52d2f4c90063083b4c656b012b2eacc39f8f76a8
bert hubert <bert@hubertnet.nl>
2024-04-22 16:07:59 +0200

    add a lot of comments

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # parsing
 Playground for examples how to use cpp-peglib.
 
-Install libfmt-dev/fmt-devel first.
+Install libfmt-dev/fmt-devel first, as well as nlohmann-json3-dev
+
+
 
 
--- promparser.cc
+++ promparser.cc
@@ -2,16 +2,15 @@
 #include "peglib.h"
 #include <fmt/ranges.h>
 using namespace std;
-
   
 PromParser::PromParser()
 {
   d_p = std::make_unique<peg::parser>();
-  auto& p = *d_p;
-  
+  auto& p = *d_p; // saves bit of typing
+
   d_p->set_logger([](size_t line, size_t col, const string& msg, const string &rule) {
     fmt::print("line {}, col {}: {}\n", line, col,msg, rule);
-  });
+  }); // gets us some helpful errors if the grammar is wrong
   
   auto ok = d_p->load_grammar(R"(
 root          <- ( ( commentline / vline ) '\n')+
@@ -20,80 +19,82 @@ commentline   <- ('# HELP ' name ' ' comment) /
                  ('#' comment)
 comment       <- (!'\n' .)*
 vline         <- (name ' ' value (' ' timestamp)?)  /
-                 (name sels ' ' value (' ' timestamp)?) 
+                 (name labels ' ' value (' ' timestamp)?) 
 name          <- [a-zA-Z0-9_]+ 
-sels          <- '{' nvpair (',' nvpair)* '}' 
+labels        <- '{' nvpair (',' nvpair)* '}' 
 nvpair        <- name '=' '"' label_value '"' 
 label_value   <- (!'"' char)*
-char          <- ('\\' . ) / (!'\\' .)
+char          <- ('\\' . ) /
+                 (!'\\' .)
 value         <- '+Inf' / '-Inf' / 'NaN' / [0-9.+e-]+ 
-timestamp     <- [+-]?[1-9][0-9]*
+timestamp     <- [+-]?[0-9]*
 )" );
 
-  //  [-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)? 
-  
-  if(!ok) {
+  if(!ok) 
     throw runtime_error("Error in grammar\n");
-  }
+
+  // this creates an attractive error messsage in case of a problem, and stores it
+  // so we can throw a useful exception later if parsing fails
   d_p->set_logger([this](size_t line, size_t col, const string& msg) {
     d_error = fmt::format("Error on line {}:{} -> {}", line, col, msg);
   });
 
+  // This contains a comment line, where choice 0 is "HELP", choice 1 is "TYPE"
+  // choice 2 is random comment, which we ignore
   struct CommentLine
   {
     size_t choice;
     string name;
     string comment;
   };
-  
+  // here we parse a comment line, and return a CommentLine
   p["commentline"] = [](const peg::SemanticValues &vs) {
-    if(vs.choice() == 0) {
+    if(vs.choice() == 0) 
       return CommentLine({vs.choice(), std::any_cast<string>(vs[0]), std::any_cast<string>(vs[1])});
-    }
-    else if(vs.choice() == 1) {
+    else if(vs.choice() == 1) 
       return CommentLine({vs.choice(), std::any_cast<string>(vs[0]), std::any_cast<string>(vs[1])});
-    }
+
     return CommentLine({vs.choice(), string(), string()});
   };
 
+  // this merely returns the comment contents as a string
   p["comment"] = [](const peg::SemanticValues &vs) {
      return vs.token_to_string();
   };
-
+  // and similar for the 'name' rule
   p["name"] = [](const peg::SemanticValues &vs) {
     return vs.token_to_string();
   };
-
+  // this is where deal with the un-escaping, using the choice()
   p["char"] = [](const peg::SemanticValues &vs) {
     string res = vs.token_to_string();
     if(vs.choice() == 0) { // this was an escape
       char c = res.at(1);
-      if(c != '\\' && c != 'n' && c!='"')
+      if(c != '\\' && c != 'n' && c != '"')
 	throw runtime_error(fmt::format("Unknown escape sequence '\\{}'", c));
-      if(c == 'n') {
+      if(c == 'n') 
 	c = '\n';
-      }
       return c;
     }
     return res.at(0);
   };
-
+  // here we assemble all the "char"'s from above into a label_value string
   p["label_value"] = [](const peg::SemanticValues &vs) {
     string ret;
     for(const auto& v : vs)
       ret.append(1, std::any_cast<char>(v));
     return ret;
   };
-
+  // only invoked if a timestamp was passed
   p["timestamp"] = [](const peg::SemanticValues &vs) {
     return vs.token_to_number<int64_t>();
   };
-
+  // combines a label key="value" pair into a std::pair<string,string>
   p["nvpair"] = [](const peg::SemanticValues &vs) {
     return std::make_pair(std::any_cast<string>(vs[0]), std::any_cast<string>(vs[1]));
   };
-
-  p["sels"] = [](const peg::SemanticValues &vs) {
+  // gathers all these pairs into a map<string,string>
+  p["labels"] = [](const peg::SemanticValues &vs) {
     map<string,string> m;
     for(const auto& sel : vs) {
       const auto p = std::any_cast<pair<string,string>>(sel);
@@ -101,9 +102,9 @@ timestamp     <- [+-]?[1-9][0-9]*
     }
     return m;
   };
-
+  // detects if a numerical value is perhaps +Inf, -Inf, NaN or a floating point value
   p["value"] = [](const peg::SemanticValues &vs) {
-    if(vs.choice() == 0 )  // +Inf
+    if(vs.choice() == 0 )       // +Inf
       return numeric_limits<double>::infinity();
     else if(vs.choice() == 1 )  // -Inf
       return -numeric_limits<double>::infinity();
@@ -112,8 +113,8 @@ timestamp     <- [+-]?[1-9][0-9]*
     
     return vs.token_to_number<double>();
   };
-
-  struct Details
+  // this reflects the contents of a vline
+  struct VlineDetails
   {
     string name;
     map<string, string> labels;
@@ -121,37 +122,36 @@ timestamp     <- [+-]?[1-9][0-9]*
     int64_t tstampmsec = 0;
   };
 
-  /*
+  /* Deals with the two choices, a line witout/without labels
   vline         <- (name ' ' value (' ' timestamp)?)  /
-                   (name sels ' ' value (' ' timestamp)?) 
+                   (name labels ' ' value (' ' timestamp)?) 
   */
   
   p["vline"] = [](const peg::SemanticValues &vs) {
-    Details d;
+    VlineDetails d;
     unsigned int pos = 0;
-    d.name = std::any_cast<string>(vs[pos]);
-    pos++;
+    d.name = std::any_cast<string>(vs[pos++]);
+
     if(vs.choice() == 1) {
-      d.labels = std::any_cast<decltype(d.labels)>(vs[pos]);
-      pos++;
+      d.labels = std::any_cast<decltype(d.labels)>(vs[pos++]);
     }
-    d.value = std::any_cast<double>(vs[pos]);
-    pos++;
+    d.value = std::any_cast<double>(vs[pos++]);
+
     if(pos < vs.size()) {
-      d.tstampmsec = std::any_cast<int64_t>(vs[pos]);
+      d.tstampmsec = std::any_cast<int64_t>(vs[pos++]);
     }
     return d;
   };
-
-
+  // this is the first rule, and the one that ::parse will return
+  // root consists of an array of VlineDetails and CommentLines
+  // which we join together in the promparseres_t map
   p["root"] = [](const peg::SemanticValues &vs)  {
     promparseres_t ret;
     for(const auto& v : vs) {
-      if(auto dptr = std::any_cast<Details>(&v)) {
+      if(auto dptr = std::any_cast<VlineDetails>(&v)) {
 	ret[dptr->name].vals[dptr->labels]={dptr->tstampmsec, dptr->value};
       }
       else if(auto cptr = std::any_cast<CommentLine>(&v)) {
-
 	if(cptr->choice == 0)
 	  ret[cptr->name].help = cptr->comment;
 	else if(cptr->choice == 1)
@@ -159,7 +159,7 @@ timestamp     <- [+-]?[1-9][0-9]*
 	// ignore random comments (choice == 2)
       }
     }
-    return ret;
+    return ret; 
   };
 }
 
@@ -171,6 +171,4 @@ PromParser::promparseres_t PromParser::parse(const std::string& in)
   return ret;
 }
 
-
-PromParser::~PromParser()
-{}
+PromParser::~PromParser(){} // needed -here- because of std::unique_ptr<>
